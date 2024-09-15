922f3e8384f058dab7f4ccfb3d89c9c0e0a4e0a8
radeveljic99 <80167674+radeveljic99@users.noreply.github.com>
2024-09-15 17:55:47 +0200

    updates performances of logistic regression model

--- lr.py
+++ lr.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when
-from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import col, when, regexp_extract
+from pyspark.ml.feature import VectorAssembler, StringIndexer
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
 
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     train_df = spark.read.csv("train.csv", header=True, inferSchema=True)
     test_df = spark.read.csv("test.csv", header=True, inferSchema=True)
 
-    # Preprocessiranje podataka
     # Racunamo srednju vrijednost za kolonu godine i dje je null popunimo sa srednjom vrijednoscu
     mean_age = train_df.agg({"Age": "mean"}).collect()[0][0]
-    train_df = train_df.fillna({'Age': mean_age, 'Embarked': 'S'}) # Embarked zamjenujemo sa S jer je vecina u Southampton
+    train_df = train_df.fillna({'Age': mean_age, 'Embarked': 'S'}) # Embarked zamjenujemo sa S dje ne postoji jer je vecina u Southampton
     test_df = test_df.fillna({'Age': mean_age, 'Embarked': 'S'})
 
     # Radimo castovanje kolona sa pravim vrijednostima
@@ -29,23 +28,42 @@ if __name__ == "__main__":
                      .withColumn("Fare", col("Fare").cast("double"))
 
 
-    # print(train_df.agg({"Fare": "mean"}).collect())
-    
     # Nakon castovanja i za ostale vrijednosti popunjavamo dje je null ili None
     train_df = train_df.fillna({'Pclass': 3, 'Fare': train_df.agg({"Fare": "mean"}).collect()[0][0], 'SibSp': 0, 'Parch': 0})
     test_df = test_df.fillna({'Pclass': 3, 'Fare': test_df.agg({"Fare": "mean"}).collect()[0][0], 'SibSp': 0, 'Parch': 0})
-    
+
     # pol pretvaramo u 0 i 1
     train_df = train_df.withColumn("Sex", when(col("Sex") == "male", 1).otherwise(0))
     test_df = test_df.withColumn("Sex", when(col("Sex") == "male", 1).otherwise(0))
 
+    # Preprocesiranje kolone Cabin
+    # Ekstrahovanje prvog karaktera kabine kao tip kabine (npr. 'C', 'B', 'D')
+    train_df = train_df.withColumn("Cabin", regexp_extract(col("Cabin"), '([A-Za-z])', 1))
+    test_df = test_df.withColumn("Cabin", regexp_extract(col("Cabin"), '([A-Za-z])', 1))
+
+
+    # Zamena null kabina sa najčešćim tipom kabine (npr. 'C')
+    filtered_train_df = train_df.filter(col("Cabin").isNotNull())
+
+    # Find the most common cabin from the filtered dataset
+    most_common_cabin_row = filtered_train_df.groupBy("Cabin").count().orderBy(col("count").desc()).first()
+    most_common_cabin = most_common_cabin_row[0] if most_common_cabin_row else "C"  # Default to 'C' if no common cabin found
+
+    train_df = train_df.fillna({'Cabin': most_common_cabin})
+    test_df = test_df.fillna({'Cabin': most_common_cabin})
+
+    # Pretvaranje tipova kabine u numerički format koristeći StringIndexer
+    indexer = StringIndexer(inputCol="Cabin", outputCol="CabinIndex")
+
+    train_df = indexer.fit(train_df).transform(train_df)
+    test_df = indexer.fit(test_df).transform(test_df)
 
     # Prebacanje datih kolona u features vektor
     assembler = VectorAssembler(
-        inputCols=["Pclass", "Age", "SibSp", "Parch", "Fare"],
+        inputCols=["Pclass", "Age", "SibSp", "Parch", "Fare", "Sex", "CabinIndex"],
         outputCol="features"
     )
-    
+
     # Transformisanje
     train_df = assembler.transform(train_df)
     test_df = assembler.transform(test_df)
@@ -56,7 +74,7 @@ if __name__ == "__main__":
 
     # model.transform vraca novi dataframe koji cuvamo u predictions df
     train_predictions = model.transform(train_df)
-    
+
     # Testiranje na training_set
     evaluator = BinaryClassificationEvaluator(labelCol="Survived")
     train_accuracy = evaluator.evaluate(train_predictions)
@@ -74,7 +92,7 @@ if __name__ == "__main__":
     # Radimo castovanje u int i micemo sve kolone koje eventualno imamo da su null ili None
     submission_df = submission_df.withColumn("Survived", col("Survived").cast("integer"))
     submission_df = submission_df.na.drop()
-        
-    # cuvanje u csv ( ovdje se napravi vise fajlova, jer moze da ima vise particija)    
+
+    # cuvanje u csv ( ovdje se napravi vise fajlova, jer moze da ima vise particija)
     submission_df.write.csv('submission-lr', header=True, mode="overwrite")
     spark.stop()
